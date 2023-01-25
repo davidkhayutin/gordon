d2a2176b769d0bb1c420a0731f54be9f2263e007
David Khayutin <david.khayutin@coursehero.com>
2023-01-25 10:02:29 -0800

    add pages

---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, { Head, Main, NextScript, Html } from 'next/document'
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: any) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return initialProps
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Rubik+Dirt&family=Secular+One&display=swap&family=Montserrat"
+                    rel="stylesheet"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <link rel="manifest" href="/site.webmanifest"></link>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
